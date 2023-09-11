986a5a93baac8490a0fafaf1fe2c6459a5e61e44
JeYeongR <wpdud2003@gmail.com>
2023-09-11 14:12:38 +0900

    Fix: thread_comments DDL

--- db/migrations/20230911021929_create_thread_comments_table.sql
+++ db/migrations/20230911021929_create_thread_comments_table.sql
@@ -1,13 +1,15 @@
 -- migrate:up
-CREATE TABLE thread_likes (
+CREATE TABLE thread_comments (
   id INT NOT NULL AUTO_INCREMENT,
-  user_id INT NOT NULL,
   thread_id INT NOT NULL,
-  created_at TIMESTAMP NOT NULL DEFAULT NOW(),           
+  user_id INT NOT NULL,
+  content VARCHAR(2000) NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT NOW(),          
+  updated_at TIMESTAMP NULL ON UPDATE CURRENT_TIMESTAMP, 
   PRIMARY KEY (id),
   FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
   FOREIGN KEY (thread_id) REFERENCES threads (id) ON DELETE CASCADE
 );
 
 -- migrate:down
-DROP TABLE thread_likes;
+DROP TABLE thread_comments;

a5b116d78b5ca8947b7fc0518c3ed658b193c5a2
JeYeongR <wpdud2003@gmail.com>
2023-09-12 16:09:47 +0900

    Feature: nickname 빈 값이면 이메일 아이디 부여

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,15 +2,15 @@ const userService = require("../services/userService");
 
 const signUp = async (req, res) => {
   const {
-    nickname,
     email,
     password,
     phoneNumber,
     birthday,
     profileImage = "https://file.notion.so/f/f/fc7a0770-8294-4680-9cb3-c81efe407127/b5f725e6-ab7c-44cc-ad87-1214e26017a9/Untitled.jpeg?id=9589c573-1bbb-48d7-a06b-a0502555d9cd&table=block&spaceId=fc7a0770-8294-4680-9cb3-c81efe407127&expirationTimestamp=1694498400000&signature=HaKy6Iu4eI1EDtDNukBMxEpac-uWJ7yl4E4vx147ODo&downloadName=Untitled.jpeg",
   } = req.body;
+  const nickname = req.body.nickname || email.substring(0, email.indexOf("@"));
 
-  if (!email || !password || !nickname) {
+  if (!email || !password) {
     const error = new Error("KEY_ERROR");
     error.status = 400;
     throw error;

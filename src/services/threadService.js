a8aff2f883314cb987915615fa17976b08706d9e
JeYeongR <wpdud2003@gmail.com>
2023-09-15 15:13:53 +0900

    Fix: 쓰레드 삭제 기능 url 파라미터로 받게 수정

--- src/services/threadService.js
+++ src/services/threadService.js
@@ -45,7 +45,6 @@ const modifyThreads = async (foundUser, threadId, content) => {
 const threadDelete = async (threadId, userId) => {
   //  Error handling - 존재하지 않는 쓰레드를 삭제하는 경우
   const existingThread = await threadDao.findThreadById(threadId);
-  console.log(existingThread);
   if (existingThread.length == 0) {
     const err = new Error("CONTENT_NOT_FOUND");
     err.status = 400;
@@ -55,7 +54,6 @@ const threadDelete = async (threadId, userId) => {
 
   //  Error handling - 다른 유저가 쓴 쓰레드를 삭제하는 경우
   const thisUserThread = await threadDao.findThreadUser(userId);
-  console.log(thisUserThread);
   if (existingThread.length > 0 && thisUserThread.length == 0) {
     const err = new Error("NO_ACCESS");
     err.status = 400;
